a29f89994b5e6886614e32cdc42faf9e5862962a
Burhanaddinzm <burhanaddinzm@code.edu.az>
2024-02-17 13:55:19 +0400

    Turned off safety

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
 
 const textareaEl = document.querySelector("textarea");
 // const btnEl = document.querySelector("button");
@@ -12,7 +13,30 @@ const genAI = new GoogleGenerativeAI(API_KEY);
 const timeBetween = 5;
 
 const run = async (inputValue) => {
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+  ];
+
+  const model = genAI.getGenerativeModel({
+    model: "gemini-pro",
+    safetySettings,
+  });
 
   const prompt = inputValue;
 
